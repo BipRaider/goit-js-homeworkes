79761c0aea09cee6b4e4ff20706922c03ad1139a
BipRaider <thebipus@gmail.com>
2020-03-10 12:46:15 +0200

    end task-06

--- module-01/js/modules/task-06.js
+++ module-01/js/modules/task-06.js
@@ -1,1 +1,21 @@
-1;
+'use strict';
+let input;
+let total = 0;
+let value;
+
+do {
+	input = prompt(`В ведите числоыва ${total}`); //делаем запрос на число
+
+	value = Number.isNaN(Number(input)); // определяем инпут на булл и присваиваем булл щначение
+	if (input === null) {
+		// если нажмут cancel
+		alert(`Общая сумма чисел равна ${total}`);
+		break;
+	} else if (value === true || Number(input) === NaN) {
+		// если в вели не число
+		alert(`Общая сумма чисел равна ${total}`);
+		break;
+	}
+
+	total += Number(input); //  прибовляем к тотал приобразованое число
+} while (value === false); // отправляем сюда инпут с булевым значением
