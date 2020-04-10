59b5a6f0a37349db85e4b7578688475d6f52e63d
BipRaider <thebipus@gmail.com>
2020-04-10 18:56:41 +0300

    fix

--- module-01/js/modules/task-03.js
+++ module-01/js/modules/task-03.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const ADMIN_PASSWORD = 'admin';
-let user = prompt(' В ведите пороль');
+let user = prompt(' В ведите пaроль');
 
 if (user === null) {
 	alert('Отменено пользователем!');
 } else if (ADMIN_PASSWORD === user) {
 	alert('Добро пожаловать!');
-} else (ADMIN_PASSWORD !== user) {
+} else if (ADMIN_PASSWORD !== user) {
 	alert('Доступ запрещен, неверный пароль!');
 }
 
