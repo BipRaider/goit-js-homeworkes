535c86f78293d0d9d4c02b78d7c901f056a55f8f
BipRaider <thebipus@gmail.com>
2020-04-23 12:06:41 +0300

    end job on task-2

--- module-03/js/task/task-2.js
+++ module-03/js/task/task-2.js
@@ -1,8 +1,16 @@
 'use strict';
 // Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
-
-const countProps = function(obj) {
-	// твой код
+console.log('start_______________task-2');
+const countProps = function(object) {
+	let counter = 0; // создали счётчик для подсчёта
+	for (const key in object) {
+		// пропускаем через цикл объект
+		if (key !== true) {
+			// проверяем на валидность , пока поступают свойства а не undefined, увеличиваем счетчик на единицу
+			counter += 1;
+		}
+	}
+	return counter; // возвращаем счетчик если в цикле закончит перебор
 };
 
 /*
@@ -13,3 +21,4 @@ console.log(countProps({})); // 0
 console.log(countProps({ name: 'Mango', age: 2 })); // 2
 
 console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+console.log('end_______________task-2');
